580298c047a4c2130aa72e7d5765a6d45bc6cde2
cem-gokcek <80675455+cem-gokcek@users.noreply.github.com>
2021-04-04 21:51:11 +0300

    Add files via upload

---
+++ Assg1.py
@@ -0,0 +1,4 @@
+Chatbot = input ("Hi, I am a chatbot. Please state your name: ")
+print("Thank you, ",Chatbot, "How I can help you?")
+inputuser = input ("User: ")
+print('Searching for', '"' , inputuser, '"')
