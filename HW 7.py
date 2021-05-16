4f3fe323a853941bebdaba3e7b776083048a1e23
cem-gokcek <80675455+cem-gokcek@users.noreply.github.com>
2021-05-16 17:03:57 +0300

    Add files via upload

---
+++ HW 7.py
@@ -0,0 +1,26 @@
+def GuestList():
+    #declare a list of six friends
+    friends = ["Ross","Rachel","Chandler","Monica","Joey","Phoebe"]
+    guestList = []
+    #loop through the list
+    for friend in friends:
+        #ask the user to enter guest in each list
+        print("Enter guest list for ",friend,":")
+        lst = input().split(" ")
+        #append the lists in guestList as a set
+        guestList.append(set(lst))
+    #declare a list to store all the friends
+    unified_list = []
+    #extract each set, take each of its element and put them into list
+    for guests in guestList:
+        for i in range(len(guests)):
+            unified_list.append(list(guests)[i])
+    #make a set of that list
+    unified_set = set(unified_list)
+    #remove the first element because the first element will be an empty string
+    unified_set.remove(list(unified_set)[0])
+    #return the set
+    return unified_set
+if __name__ == "__main__":
+    #call and print the set
+    print("The unified set of guest is:\n",GuestList())
